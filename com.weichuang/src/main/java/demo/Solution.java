793e5fb8f5b59825a89ef470b9c48209d1e73f8c
杨一龙 <yyl20020827@foxmail.com>
2024-12-09 17:58:13 +0800

    leecode

--- com.weichuang/src/main/java/demo/Solution.java
+++ com.weichuang/src/main/java/demo/Solution.java
@@ -1,41 +1,61 @@
 package demo;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 class Solution {
-    /*给定两个字符串 text1 和 text2，返回这两个字符串的最长 公共子序列 的长度。如果不存在 公共子序列 ，返回 0 。
-      一个字符串的 子序列 是指这样一个新的字符串：它是由原字符串在不改变字符的相对顺序的情况下删除某些字符（也可以不删除任何字符）后组成的新字符串。
-      例如，"ace" 是 "abcde" 的子序列，但 "aec" 不是 "abcde" 的子序列。
-      两个字符串的 公共子序列 是这两个字符串所共同拥有的子序列。
 
-      示例 1：
-      输入：text1 = "abcde", text2 = "ace"
-      输出：3
-      解释：最长公共子序列是 "ace" ，它的长度为 3 。
+    /*给你一个 非空 整数数组 nums ，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+    你必须设计并实现线性时间复杂度的算法来解决此问题，且该算法只使用常量额外空间。
+    示例 1 ：
+    输入：nums = [2,2,1]
+    输出：1
 
-      示例 2：
-      输入：text1 = "abc", text2 = "abc"
-      输出：3
-      解释：最长公共子序列是 "abc" ，它的长度为 3 。
+    示例 2 ：
+    输入：nums = [4,1,2,1,2]
+    输出：4
 
-      示例 3：
-      输入：text1 = "abc", text2 = "def"
-      输出：0
-      解释：两个字符串没有公共子序列，返回 0 。*/
-    public int longestCommonSubsequence(String text1, String text2) {
-        int maxLen = 0;
-        int[][] dp = new int[text1.length() + 1][text2.length() + 1];
-        for (int i = 0; i <= text1.length(); i++) {
-            for (int j = 0; j <= text2.length(); j++) {
-                if (i == 0 || j == 0) {
-                    dp[i][j] = 0;
-                } else if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
-                } else {
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
-                }
-            }
+    示例 3 ：
+    输入：nums = [1]
+    输出：1*/
+    public int singleNumber(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+        Map<Integer,Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+           int times = map.getOrDefault(nums[i],0);
+           map.put(nums[i],times+1);
         }
-        return dp[text1.length()][text2.length()];
 
+        map.forEach((k,v) -> {
+            if (v==1){
+                res.add(k);
+            }
+        });
+        return res.get(0);
+    }
+    /*给定一个大小为 n 的数组 nums ，返回其中的多数元素。多数元素是指在数组中出现次数 大于 ⌊ n/2 ⌋ 的元素。
+    你可以假设数组是非空的，并且给定的数组总是存在多数元素。
+    示例 1：
+    输入：nums = [3,2,3]
+    输出：3
 
+    示例 2：
+    输入：nums = [2,2,1,1,1,2,2]
+    输出：2*/
+    public int majorityElement(int[] nums) {
+        int len = nums.length;
+        int res =nums[0];
+        Map<Integer,Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int count = map.getOrDefault(map.get(nums[i]),0);
+            if (++count >= len/2){
+                res = nums[i];
+                break;
+            }
+            map.put(nums[i],count);
+        }
+        return res;
     }
 }
